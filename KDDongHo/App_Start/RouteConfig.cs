9c609814d696af4e13a25654312fe0cbea17d6ad
Phuong <arials93@gmail.com>
2019-05-11 21:04:39 +0700

    first commit

---
+++ KDDongHo/App_Start/RouteConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace KDDongHo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            #region Webhome
+            routes.MapRoute(
+                name: "WebIndex",
+                url: "{controller}/{action}",
+                defaults: new { controller = "WebHome", action = "Index" }
+            );
+            #endregion
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "AdminDashboard", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Product",
+                url: "{controller}/{action}",
+                defaults: new { controller = "AdminDashboard", action = "Product" }
+            );
+
+            routes.MapRoute(
+                name: "AddProduct",
+                url: "{controller}/{action}",
+                defaults: new { controller = "AdminDashboard", action = "AddProduct" }
+            );
+
+            routes.MapRoute(
+               name: "Brand",
+               url: "{controller}/{action}",
+               defaults: new { controller = "AdminDashboard", action = "Brand" }
+           );
+
+            routes.MapRoute(
+              name: "Series",
+              url: "{controller}/{action}",
+              defaults: new { controller = "AdminDashboard", action = "Series" }
+          );
+        }
+    }
+}
---
+++ KDDongHo/Controllers/WebHomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KDDongHo.Models;
+
+namespace KDDongHo.Controllers
+{
+    public class WebHomeController : Controller
+    {
+        QLKD_DONGHOEntities db = new QLKD_DONGHOEntities();
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+	}
+}
